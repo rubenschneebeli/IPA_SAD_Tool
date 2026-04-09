3b5a7e68cab1b0d45ee6fc6e13296b0eecb29945
ruben.schneebeli <ruben.schneebeli@accenture.com>
2026-04-09 08:22:02 +0200

    feat: started project setup

---
+++ SAD-Tool/backend/src/main/java/com/accenture/sad_tool/SadToolApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.sad_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SadToolApplication.class, args);
+	}
+
+}
