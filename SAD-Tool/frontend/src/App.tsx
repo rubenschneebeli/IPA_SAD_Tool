e80ba85082807e6ec0b9547e3ba0e71517f744cd
ruben.schneebeli <ruben.schneebeli@accenture.com>
2026-04-16 11:04:59 +0200

    added template edit page with new richtext editor and update + save function

--- SAD-Tool/frontend/src/App.tsx
+++ SAD-Tool/frontend/src/App.tsx
@@ -3,6 +3,7 @@ import LoginPage from "./pages/LoginPage"
 import { AuthProvider, useAuth } from "./AuthContext"
 import TemplatesPage from "./pages/TemplateOverviewPage"
 import DocumentOverview from "./pages/DocumentOverviewPage"
+import TemplateEditorPage from "./pages/TemplateEditorPage"
 
 function PrivateRoute({ children }: { children: React.ReactNode }) {
   const { isLoggedIn } = useAuth()
@@ -17,7 +18,9 @@ function App() {
        <Route path="/" element={<Navigate to="/login" replace />} />
        <Route path="/login" element={<LoginPage />} />
        <Route path="/templates" element={<PrivateRoute><TemplatesPage /></PrivateRoute>} />
-
+       <Route path="/templates/new" element={<PrivateRoute><TemplateEditorPage /></PrivateRoute>} />
+       <Route path="/templates/:id/edit" element={<PrivateRoute><TemplateEditorPage /></PrivateRoute>} />
+       
        <Route path="/documents" element={<PrivateRoute><DocumentOverview /></PrivateRoute>} />
 
        </Routes>
